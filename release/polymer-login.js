bd57f4bb4555df549a9f71ef497f982cb64514c5
Mika Turunen <mika.turunen@ymail.com>
2015-07-17 10:31:28 +0300

    Create login/logout behavior with default values.

--- release/polymer-login.js
+++ release/polymer-login.js
@@ -1,7 +1,37 @@
 "use strict";
 Polymer({
     is: "polymer-login",
+    properties: {
+        loginRoute: {
+            type: String,
+            value: "/user/login"
+        },
+        logoutRoute: {
+            type: String,
+            value: "/user/login"
+        },
+        silence: {
+            type: Boolean,
+            value: true
+        }
+    },
     openLoginDialog: function () {
         this.$.LoginDialog.open();
+        this.$.InputUserName.focus();
+        if (!this.silence) {
+            console.log(this.loginRoute, this.logoutRoute);
+        }
+    },
+    tryLogin: function () {
+        if (!this.silence) {
+            console.log("Attempting to login the user through route:", this.loginRoute);
+        }
+        this.$.TryLoging.generateRequest();
+    },
+    tryLogout: function () {
+        if (!this.silence) {
+            console.log("Attempting to logout the user through route:", this.logoutRoute);
+        }
+        this.$.TryLogout.generateRequest();
     }
 });
--- typescript/polymer-login.ts
+++ typescript/polymer-login.ts
@@ -1,14 +1,57 @@
 "use strict";
 
 Polymer({
-    /**
-     * What component we are attached to.
-     */
+    /** * What component we are attached to. */
     is: "polymer-login",
 
-    /** Opens Login dialog */
+    properties: {
+        /** {string} Login route the component should use when attempting to login the user. */
+        loginRoute: {
+            type: String,
+            // TODO change default to /login after we've confirmed the attribute work
+            value: "/user/login"
+        },
+
+        /** {string} Logout route the component should use when attemping to logout the user. */
+        logoutRoute: {
+            type: String,
+            value: "/user/login"
+        },
+
+        silence: {
+            type: Boolean,
+            value: true
+        }
+    },
+
+    /**
+     * Opens Login dialog.
+     */
     openLoginDialog: function() {
         this.$.LoginDialog.open();
-        // this.$.Username.focus();
+        this.$.InputUserName.focus();
+
+        if (!this.silence) {
+            console.log(this.loginRoute, this.logoutRoute);
+        }
+    },
+
+    /**
+     * Attempts to login into the system through the provided REST route.
+     */
+    tryLogin: function() {
+        if (!this.silence) {
+            console.log("Attempting to login the user through route:", this.loginRoute);
+        }
+
+        this.$.TryLoging.generateRequest();
+    },
+
+    tryLogout: function() {
+        if (!this.silence) {
+            console.log("Attempting to logout the user through route:", this.logoutRoute);
+        }
+
+        this.$.TryLogout.generateRequest();
     }
 });
